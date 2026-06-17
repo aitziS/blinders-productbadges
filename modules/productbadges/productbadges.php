d245aaff40078dc544e4408b3cbdeb112c2dcc41
Aitziber Solís <aitzisolba@gmail.com>
2026-06-17 13:00:01 +0100

    registro de pestaña de administración en install/uninstall. PENDIENTE: contenido de AdminProductBadgesController

--- modules/productbadges/productbadges.php
+++ modules/productbadges/productbadges.php
@@ -43,15 +43,41 @@ class ProductBadges extends Module
             return false;
         }
 
+        // Registrar pestaña de administración
+        if (!Tab::getIdFromClassName('AdminProductBadgesController')) {
+            $tab = new Tab();
+            $tab->class_name = 'AdminProductBadgesController';
+            $tab->module = $this->name;
+            $tab->id_parent = (int) Tab::getIdFromClassName('AdminCatalog');
+            $tab->active = 1;
+            foreach (Language::getLanguages(false) as $lang) {
+                $tab->name[$lang['id_lang']] = $this->l('Product Badges');
+            }
+
+            if (!$tab->add()) {
+                // Rollback si la pestaña no pudo crearse
+                parent::uninstall();
+                return false;
+            }
+        }
+
         return true;
     }
 
     public function uninstall()
     {
+        // Eliminar pestaña de administración antes de desinstalar el módulo
+        $id_tab = Tab::getIdFromClassName('AdminProductBadgesController');
+        if ($id_tab) {
+            $tab = new Tab((int) $id_tab);
+            $tab->delete();
+        }
+
         if (!parent::uninstall()) {
             return false;
         }
 
+        // Ejecutar eliminación de tablas
         include_once __DIR__ . '/sql/uninstall.php';
         if (function_exists('productbadges_uninstall_sql')) {
             return (bool) productbadges_uninstall_sql();
